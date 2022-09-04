06ce28a60b8c8f18b1fb3c28e909c4dc5196c390
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-04 14:54:34 +0530

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Button from './Button';
 const Navbar = () => {
 
   let Links = [
     { name: "HOME", link: "/" },
-    { name: "SERVICE", link: '/' },
     { name: "ABOUT", link: '/' },
-    { name: "BLOG'S", link: '/' },
-    { name: "CONTACT", link: '/' },
+    { name: "MEDIA", link: '/' },
+    { name: "DELHI GOVERNMENT", link: '/' },
+    { name: "ELECTIONS", link: '/' },
+    { name: "GET INVOLVED", link: '/' },
   ];
 
   const [open, setOpen] = useState(false);
@@ -19,14 +20,28 @@ const Navbar = () => {
           <span className='text-3xl text-indigo-600 mr-1 pt-2'>
             <ion-icon name="logo-ionic"></ion-icon>
           </span>
-          Designer
+          AAM AADMI PARTY
         </div>
 
         <div onClick={() => setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
           <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
         </div>
 
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white m`}></ul>
+        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
+          {
+            Links.map((link) => (
+              <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
+                <a href={link.link} className='text-gray-800 hover:text-gray-400 duration-500'>
+                  {link.name}
+                </a>
+              </li>
+            ))
+          }
+
+          <Button>
+            DONATE
+          </Button>
+        </ul>
       </div>
     </div>
   )
