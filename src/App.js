0af77a1edab0b4111454b772ca0f5e449a799943
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-06 15:40:08 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Footer } from './components';
+import { Navbar, Footer } from './components/index';
 
 
 
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <div className='bg-indigo-600 w-full h-screen' /> */}
+      <div className='bg-indigo-600 w-full h-screen' />
       <Footer />
     </div>
   )
