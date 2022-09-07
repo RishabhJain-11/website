7fbf15e0e90c1993f45c04a8a355fdc3536ed414
Rishabh Jain <jainrishabh71011@gmail.com>
2022-09-07 17:52:45 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Footer from './components/Footer/Footer';
-
+import Hero from './components/Hero/Hero';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <div className='bg-indigo-600 w-full h-screen' />
+      {/* <div className='bg-indigo-600 w-full h-screen' /> */}
+      <Hero />
       <Footer />
     </div>
   )
